adb8700349858b45609e54a1c4625297755a541c
DongwooKim <kimphysicsman@gmail.com>
2022-09-26 15:54:20 +0900

    feat/ 유저 모델링 & JWT 로그인 구현

--- PreOnBoarding_Service/urls.py
+++ PreOnBoarding_Service/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('users', include('user.urls')),
 ]
---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
+
+# /users
+urlpatterns = [
+    path('/login', TokenObtainPairView.as_view(), name='login')
+]
