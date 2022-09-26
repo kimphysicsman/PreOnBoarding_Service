885d51cb127c4aa38a8cf4f2bb53482b3392d091
DongwooKim <kimphysicsman@gmail.com>
2022-09-26 16:55:01 +0900

    feat/ 좋아요, 해시태그 모델 생성

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from post.models import Post as PostModel
+from post.models import (
+    Post as PostModel,
+    Like as LikeModel,
+    Hashtag as HashtagModel,
+    PostHashtag as PostHashtagModel
+)
 
 admin.site.register(PostModel)
+admin.site.register(LikeModel)
+admin.site.register(HashtagModel)
+admin.site.register(PostHashtagModel)
--- post/models.py
+++ post/models.py
@@ -3,6 +3,10 @@ from django.db import models
 from user.models import User as UserModel
 
 class Post(models.Model):
+    """
+        게시글 모델
+    """
+
     user = models.ForeignKey(UserModel, verbose_name="작성자", on_delete=models.CASCADE)
     title = models.CharField("제목" , max_length=128)
     content = models.TextField("내용", max_length=50000)
@@ -13,3 +17,38 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.title}"
+
+
+class Like(models.Model):
+    """
+        좋아요 모델
+    """
+    
+    user = models.ForeignKey(UserModel, verbose_name="등록자", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name="게시글", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id} - {self.post.title} - {self.user.username}"
+
+
+class Hashtag(models.Model):
+    """
+        해시태그 모델
+    """
+
+    name = models.CharField("해시태그명", max_length=128)
+    post = models.ManyToManyField(Post, verbose_name="게시글", through="PostHashtag")
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
+class PostHashtag(models.Model):
+    """
+        게시글 - 해시태그 중간 테이블
+    """
+
+    hashtag = models.ForeignKey(Hashtag, verbose_name="해시태그", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name="게시글", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id} - {self.post.title} - {self.hashtag.name}"
