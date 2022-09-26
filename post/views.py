a76f8c2dbd9d6b8a16da74648a4743cc6f846854
DongwooKim <kimphysicsman@gmail.com>
2022-09-26 17:39:15 +0900

    refac/ 파일명 수정 & JWT 설정 추가

--- post/views.py
+++ post/views.py
@@ -2,9 +2,13 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from post.serializers import (
+    PostModelSerializer    
+)
 
 # 게시글 View
 class PostView(APIView):
     # 게시글 생성
     def post(self, request):
+        
         return Response({}, status=status.HTTP_200_OK)
